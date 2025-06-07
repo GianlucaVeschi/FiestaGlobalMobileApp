39fd7ef3985b23e9ec84cd7eecb16fd2aac0957c
GianlucaVeschi <Gianluca.veschi@getyourguide.com>
2025-06-07 17:27:10 +0200

    Move hardcodedArtists

--- composeApp/src/androidMain/kotlin/org/gianlucaveschi/fiestaglobal/ui/screens/profile/ArtistsScreen.kt
+++ composeApp/src/androidMain/kotlin/org/gianlucaveschi/fiestaglobal/ui/screens/profile/ArtistsScreen.kt
@@ -1,10 +1,10 @@
 package org.gianlucaveschi.fiestaglobal.ui.screens.profile
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -20,55 +20,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.systemBarsPadding
-
-val hardcodedArtists = listOf(
-  "BANDAKADABRA",
-  "BOLOGNA BRIDGE BAND",
-  "COLORI IN MUSICA",
-  "COMPAGNIA POUET",
-  "COMPAGNIA TREDIDANE'",
-  "DUBADUBA",
-  "FOLA'",
-  "GAIA MA",
-  "GHIGO DJ",
-  "IL GIARDINO DEI COLORI",
-  "IL TEATRO DELLE ISOLE",
-  "JBEAT E MARINELLI",
-  "K SLY",
-  "KATASTROFA",
-  "LA COMPAGNIA DI VIVA EL BALL!",
-  "LA DAMA CON L'ERMELLINO",
-  "LAPALLAROTONDA",
-  "LE SGAMBATE",
-  "LION D",
-  "LUCCIARINI TRIO",
-  "MA KENLUCK",
-  "MENGO DJ",
-  "MOTSWAKO JAZZ COLLECTIVE",
-  "PACKIM DJ",
-  "PINAKOTEK",
-  "POETRY IN THE STREET",
-  "QUETZALCOATL",
-  "RAYMOND SOLFANELLI",
-  "SENZA VOLTO",
-  "SINGLE BARREL",
-  "SLACK DJSET",
-  "SLACK ERMANNO AND THE EX PRESIDENTS",
-  "SLACK LAST KILLERS",
-  "SLACK THE LINGS",
-  "SOULFIRE PROJECT",
-  "SWING O LELES",
-  "THE ANGE",
-  "TWO MEN ORCHESTRA",
-  "VERONICA GONZALEZ",
-  "VIBRA FAMILY",
-  "WORABBI E LE DOVUTE PRECAUZIONI"
-)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -122,3 +76,47 @@ fun ArtistsScreen(
     }
   }
 }
+
+val hardcodedArtists = listOf(
+  "BANDAKADABRA",
+  "BOLOGNA BRIDGE BAND",
+  "COLORI IN MUSICA",
+  "COMPAGNIA POUET",
+  "COMPAGNIA TREDIDANE'",
+  "DUBADUBA",
+  "FOLA'",
+  "GAIA MA",
+  "GHIGO DJ",
+  "IL GIARDINO DEI COLORI",
+  "IL TEATRO DELLE ISOLE",
+  "JBEAT E MARINELLI",
+  "K SLY",
+  "KATASTROFA",
+  "LA COMPAGNIA DI VIVA EL BALL!",
+  "LA DAMA CON L'ERMELLINO",
+  "LAPALLAROTONDA",
+  "LE SGAMBATE",
+  "LION D",
+  "LUCCIARINI TRIO",
+  "MA KENLUCK",
+  "MENGO DJ",
+  "MOTSWAKO JAZZ COLLECTIVE",
+  "PACKIM DJ",
+  "PINAKOTEK",
+  "POETRY IN THE STREET",
+  "QUETZALCOATL",
+  "RAYMOND SOLFANELLI",
+  "SENZA VOLTO",
+  "SINGLE BARREL",
+  "SLACK DJSET",
+  "SLACK ERMANNO AND THE EX PRESIDENTS",
+  "SLACK LAST KILLERS",
+  "SLACK THE LINGS",
+  "SOULFIRE PROJECT",
+  "SWING O LELES",
+  "THE ANGE",
+  "TWO MEN ORCHESTRA",
+  "VERONICA GONZALEZ",
+  "VIBRA FAMILY",
+  "WORABBI E LE DOVUTE PRECAUZIONI"
+)
